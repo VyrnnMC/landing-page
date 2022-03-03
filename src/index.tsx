bfe9588e1d7c7adb9e9c734603c656d4488f8c15
Vinícius Gutierrez <srgutyerrez@gmail.com>
2022-03-03 14:08:19 -0300

    feat: (✪‿✪)ノ first commit

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1
+      style={{
+        fontFamily: 'Epilogue',
+        display: 'block',
+        textAlign: 'center',
+        marginTop: '50vh',
+        fontSize: '50px'
+      }}
+    >Coming soon...</h1>
+  </React.StrictMode>,
+  document.body
+);
